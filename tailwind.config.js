f117cc4aaad9c9d43d83a9b0f1c86d6af5efd6d3
Roger Marvin <20122077@kaishi-pu.ac.jp>
2024-07-21 21:51:33 +0900

    deploying

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,ts,tsx}"],
-  darkMode: false,
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
