586e6affdd137ebc4db8070e5a4708ed37cd9389
Roger Marvin <20122077@kaishi-pu.ac.jp>
2024-07-21 22:09:26 +0900

    add

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 
@@ -7,6 +8,8 @@ import "./components/Modal/Modal.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/entype">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
