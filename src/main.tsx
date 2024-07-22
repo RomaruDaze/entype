e2a1565ca326144b995e87c159a59ba615652880
Roger Marvin <20122077@kaishi-pu.ac.jp>
2024-07-22 14:37:45 +0900

    Deploying preparation

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 
@@ -8,8 +7,6 @@ import "./components/Modal/Modal.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/entype">
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
